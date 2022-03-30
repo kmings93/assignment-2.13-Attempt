d0932301f55872fdb02468806a7a97fbc5022d3d
Kwek Ming Sheng <kmings93@gmail.com>
2022-03-30 16:35:55 +0800

    update 2

---
+++ src/config/index.js
@@ -0,0 +1,19 @@
+const env = process.env.REACT_APP_ENV;
+
+let config = {
+  baseURL: "http://localhost/",
+};
+
+switch (env.toUpperCase()) {
+  case "STAGE":
+    config.baseURL = "https://stage.skillsunion.com";
+    break;
+  case "PRODUCTION":
+    config.baseURL = "https://skillsunion.com";
+    break;
+  default:
+    console.log("Error");
+    alert("Ërror");
+}
+
+export default config;
