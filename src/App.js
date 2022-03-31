eab6d2b3fc8c7d051d4a0739c0fd57bc578998fb
Kwek Ming Sheng <kmings93@gmail.com>
2022-03-31 16:53:05 +0800

    test 3

--- src/App.js
+++ src/App.js
@@ -11,10 +11,9 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React (configuration)
+          Learn React (configuration) BaseURL: {config.baseURL}
         </a>
       </header>
-      <div> BaseURL: {config.baseURL}</div>
     </div>
   );
 }
